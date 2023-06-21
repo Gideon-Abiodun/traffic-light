95bbe97ce780bc3c4197def550c5a4b9f2d7e1c4
Gideon-Abiodun <skuriangideon@gmail.com>
2023-06-21 15:48:30 +0100

    format

--- script.js
+++ script.js
@@ -4,24 +4,24 @@ const greenLight = document.getElementById("green-light");
 
 function startSignal() {
   setTimeout(function () {
-      redLight.style.opacity = 1;
-      orangeLight.style.opacity = 0;
-      greenLight.style.opacity = 0;
-      redLight.style.display = 'block';
-    }, 1000);
+    redLight.style.opacity = 1;
+    orangeLight.style.opacity = 0;
+    greenLight.style.opacity = 0;
+    redLight.style.display = "block";
+  }, 1000);
 
   setTimeout(function () {
-      redLight.style.opacity = 0;
-      orangeLight.style.opacity = 1;
-      greenLight.style.opacity = 0;
-      orangeLight.style.display = "block";
+    redLight.style.opacity = 0;
+    orangeLight.style.opacity = 1;
+    greenLight.style.opacity = 0;
+    orangeLight.style.display = "block";
   }, 4000);
 
   setTimeout(function () {
-      redLight.style.opacity = 0;
-      orangeLight.style.opacity = 0;
-      greenLight.style.opacity = 1;
-      greenLight.style.display = "block";
+    redLight.style.opacity = 0;
+    orangeLight.style.opacity = 0;
+    greenLight.style.opacity = 1;
+    greenLight.style.display = "block";
   }, 8000);
 }
 const mainTimer = setInterval(function () {
