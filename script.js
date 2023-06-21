76995e8991cf2da70ef34cd9d250dba14a23c2fc
Gideon-Abiodun <skuriangideon@gmail.com>
2023-06-21 15:17:31 +0100

    styles

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
+const redLight = document.getElementById("red-light");
+const orangeLight = document.getElementById("orange-light");
+const greenLight = document.getElementById("green-light");
+
 function startSignal() {
-  const redLight = document.getElementById("red-light");
-  const orangeLight = document.getElementById("orange-light");
-  const greenLight = document.getElementById("green-light");
-  greenLight.style.opacity = 1;
   setTimeout(function () {
-    redLight.style.display = 'block';
-    redLight.style.opacity = 1;
-    orangeLight.style.opacity = 0;
-    greenLight.style.opacity = 0;
-  }, 1000);
+      redLight.style.opacity = 1;
+      orangeLight.style.opacity = 0;
+      greenLight.style.opacity = 0;
+      redLight.style.display = 'block';
+    }, 1000);
 
   setTimeout(function () {
-    orangeLight.style.display = "block";
-    redLight.style.opacity = 0;
-    orangeLight.style.opacity = 1;
-    greenLight.style.opacity = 0;
+      redLight.style.opacity = 0;
+      orangeLight.style.opacity = 1;
+      greenLight.style.opacity = 0;
+      orangeLight.style.display = "block";
   }, 4000);
 
   setTimeout(function () {
-    greenLight.style.display = "block";
-    redLight.style.opacity = 0;
-    orangeLight.style.opacity = 0;
-    greenLight.style.opacity = 1;
+      redLight.style.opacity = 0;
+      orangeLight.style.opacity = 0;
+      greenLight.style.opacity = 1;
+      greenLight.style.display = "block";
   }, 8000);
 }
-var mainTimer = setInterval(function () {
+const mainTimer = setInterval(function () {
   startSignal();
 }, 12000);
+
 startSignal();
